9afcc9b6d8f373223f917a24c8840fa7a99a7adb
Dan1un1u <359617626@qq.com>
2026-08-31 12:37:05 +0800

    exp0093 report correct experiment identity

--- src/host/block_main.c
+++ src/host/block_main.c
@@ -2346,7 +2346,7 @@ int main(int argc, char **argv) {
     release_result = qbh_session_release(&session);
     close_result = qbh_session_close(&session);
     printf(
-        "{\"experiment\":\"EXP-0084\","
+        "{\"experiment\":\"EXP-0093\","
         "\"execution_unit\":\"qwen3_layer14_complete_block_m64\","
         "\"variant\":\"%s\",\"attention_compute\":\"%s\","
         "\"projection_compute\":\"%s\","
