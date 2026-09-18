e33e107e7623bd18a76456004804d3be953f2fca
Dan1un1u <359617626@qq.com>
2026-09-18 17:09:14 +0800

    Generalize native long KV packing to head128 without scalar conversion

--- src/dsp/block_imp.c
+++ src/dsp/block_imp.c
@@ -20575,7 +20575,7 @@ static int qbh_scan_u8_attention(
                 return -1;
             }
             start = HAP_perf_get_qtimer_count();
-            if(header->long_prompt_tokens && (header->long_optimization&2U) && QBH_BLOCK_HEAD_DIM==64U)
+            if(header->long_prompt_tokens && (header->long_optimization&2U))
                 qbh_attention_u8_pack_k_long(plane_a,valid_tokens,padded_tokens,config,weight,qk_bias);
             else
             qbh_attention_u8_pack_k_row_major(
@@ -20751,7 +20751,7 @@ static int qbh_scan_u8_attention(
                 delta_lut_numerator=config->v_recenter_numerator;
                 delta_lut_denominator=config->v_recenter_denominator;
             }
-            if(header->long_prompt_tokens && (header->long_optimization&8U) && QBH_BLOCK_HEAD_DIM==64U)
+            if(header->long_prompt_tokens && (header->long_optimization&8U))
                 qbh_attention_u8_pack_v_long(plane_c,valid_tokens,padded_tokens,config,weight,av_bias,buffers->up,&telemetry.v_recenter_saturation_count);
             else
             qbh_attention_u8_pack_v_row_major_hvx_prepared(
