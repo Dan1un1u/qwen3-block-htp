f41c2bf93dbf68121c2cd6b53133a36d03a3ad19
Dan1un1u <359617626@qq.com>
2026-09-18 17:34:30 +0800

    Fix Qwen long-cache packer definitions in DSP library

--- src/dsp/attention_u8_core.c
+++ src/dsp/attention_u8_core.c
@@ -2774,6 +2774,7 @@ static void qbh_attention_u8_requant_softmax_dynamic_hvx_tile4(
     asm volatile("barrier" ::: "memory");
 }
 
+#include "long_kv_native.inc"
 #include "long_softmax_native.inc"
 
 static void qbh_attention_u8_dynamic_stash_scalar_probability(
