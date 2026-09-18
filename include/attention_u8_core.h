e36310b3dc29d0d0a33787cb334e7bdffcacbd1a
Dan1un1u <359617626@qq.com>
2026-09-18 22:18:45 +0800

    EXP-0297: expose fixed K transpose to vector register allocation

--- include/attention_u8_core.h
+++ include/attention_u8_core.h
@@ -40,6 +40,7 @@ void qbh_attention_u8_softmax_long_native4(uint8_t *,uint8_t *,uint32_t,uint32_t
 
 void qbh_attention_u8_softmax_long_native4_fast(uint8_t *,uint8_t *,uint32_t,uint32_t,uint32_t,const struct qbh_attention_config *);
 void qbh_attention_u8_pack_k_long_transpose(const uint8_t *,uint32_t,uint32_t,const struct qbh_attention_config *,int8_t *,uint32_t *);
+void qbh_attention_u8_pack_k_long_register(const uint8_t *,uint32_t,uint32_t,const struct qbh_attention_config *,int8_t *,uint32_t *);
 void qbh_attention_u8_pack_k_long(const uint8_t *,uint32_t,uint32_t,const struct qbh_attention_config *,int8_t *,uint32_t *);
 void qbh_attention_u8_pack_v_long(const uint8_t *,uint32_t,uint32_t,const struct qbh_attention_config *,int8_t *,uint32_t *,uint8_t *,uint32_t *);
 void qbh_attention_u8_pack_v_long_register(const uint8_t *,uint32_t,uint32_t,const struct qbh_attention_config *,int8_t *,uint32_t *,uint8_t *,uint32_t *);
