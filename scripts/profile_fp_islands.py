efe22be9e6c16f7a4a3a15009e21f0636e5dc85f
Dan1un1u <359617626@qq.com>
2026-09-21 21:20:10 +0800

    EXP0305 bind diagnostic to compatible four-context scheduler flags

--- scripts/profile_fp_islands.py
+++ scripts/profile_fp_islands.py
@@ -109,9 +109,9 @@ def execute(tag,repeat=1,audit=False,nl=28):
  base=read(R.parent/'exp0284/fixed-aux-INT16-r1/protocol.json');prefix,args=base['command'].split(' ./qwen3_block_cli ',1)
  env=dict(t.split('=',1) for t in shlex.split(prefix.split(' && ')[1]));argv=shlex.split(args);binary=REMOTE+f'/binaries-{nl}'
  # Retain vector operators, four attention contexts and ordinary group preparation.
- argv[8]='hvx_fused_post_norm_pool4';argv[15]='u8_log2_gqa';argv[16]='4';argv[18]='serial';argv[19]='hvx_tree'
+ argv[8]='hvx_fused_post_norm_pool4';argv[15]='u8_log2_gqa';argv[16]='4';argv[18]='serial';argv[19]='hvx_tree';argv[24]='0'
  for k in ['QBH_EVAL_FILE','QBH_GENERATION_AUDIT_DIR','QBH_GENERATION_BOUNDARY_AUDIT']:env.pop(k,None)
- env.update(LD_LIBRARY_PATH=binary,DSP_LIBRARY_PATH=binary,ADSP_LIBRARY_PATH=binary,QBH_GENERATION_STEPS='2',QBH_GENERATION_EXPECTED_TOKENS='64',QBH_GENERATION_SEQUENCE='9',QBH_SP2='8',QBH_WIDE_SCORE='7',QBH_PAPER_PIPELINE_DISABLE='3',QBH_DENSE_R3='0',QBH_DENSE_R4='0',QBH_W4U8_DECODE_AV_REQUANT_ROWS='4')
+ env.update(LD_LIBRARY_PATH=binary,DSP_LIBRARY_PATH=binary,ADSP_LIBRARY_PATH=binary,QBH_GENERATION_STEPS='2',QBH_GENERATION_EXPECTED_TOKENS='64',QBH_GENERATION_SEQUENCE='9',QBH_SP2='8',QBH_WIDE_SCORE='7',QBH_PAPER_PIPELINE_DISABLE='3',QBH_DENSE_R3='0',QBH_DENSE_R4='0',QBH_W4U8_DECODE_AV_REQUANT_ROWS='4',QBH_W4U8_QKV_RING_EXPAND_WORKERS='0')
  fixture=read(R/'fixture.json');words=[0x51424556,2,repeat,69]
  for j in range(repeat):words += [j,3,2]+fixture['prompt']+fixture['fixed'][:2]
  ef=d/'trajectory.bin';ef.write_bytes(struct.pack('<'+'I'*len(words),*words));er=binary+'/'+tag+'-trajectory.bin';env['QBH_EVAL_FILE']=er
